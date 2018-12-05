e1925cc2192c17f1859e72d3cf15c6dcc48b4cf9
edge <edge2@satrip.com>
2018-12-05 12:38:38 +0800

    Initial commit

---
+++ src/com/mav.java
@@ -0,0 +1,27 @@
+package com;
+
+import com.sun.org.apache.bcel.internal.generic.IFNE;
+import com.sun.org.apache.bcel.internal.generic.IfInstruction;
+import jdk.internal.org.objectweb.asm.commons.AdviceAdapter;
+import sun.java2d.d3d.D3DSurfaceData;
+
+import java.util.List;
+
+/**
+ * @author edge
+ * @date 2018/12/5-9:25
+ */
+public class mav {
+    public static void main(String[] args) {
+        System.out.println("args = [" + args + "]");
+
+
+    }
+
+    public void aa() {
+
+    }
+//    pf Debug
+//    String aa ="ddd";
+
+}
